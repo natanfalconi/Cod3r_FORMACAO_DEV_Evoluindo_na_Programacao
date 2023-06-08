38329356895608edb2fd96bb66f9a078542cbec5
natanfalconi <nfalconi65@gmail.com>
2023-06-08 20:01:41 -0300

    basico 3 - pratica

---
+++ objetos/basico_3.js
@@ -0,0 +1,16 @@
+const data = {
+    dia: 12,
+    mes: 11,
+    ano: 2023
+}
+
+data.dia = 9
+data.mes = 12
+
+console.log(data.dia)
+console.log(data.mes)
+console.log(data.ano)
+
+//interpolação
+
+console.log(`${data.dia}/${data.mes}/${data.ano}`)
